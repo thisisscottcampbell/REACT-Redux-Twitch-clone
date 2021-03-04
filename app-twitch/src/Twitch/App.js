19033b8d526d7ab4a7fb69a0665009270a091c1d
Scott Campbell <stc9486@gmail.com>
2021-03-03 23:13:23 -0500

    routing

--- app-twitch/src/Twitch/App.js
+++ app-twitch/src/Twitch/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamList from '../components/streams/StreamList';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamShow from '../components/streams/StreamShow';
+
 function App() {
-	return <div>I am app</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" exact component={StreamCreate} />
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route path="/streams/delete" exact component={StreamDelete} />
+					<Route path="/streams/show" exact component={StreamShow} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
