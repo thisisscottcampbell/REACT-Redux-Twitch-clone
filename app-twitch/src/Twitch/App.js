b927d245fe55886ae7c7640d6f31067d318d6494
Scott Campbell <stc9486@gmail.com>
2021-03-03 23:22:14 -0500

    nav

--- app-twitch/src/Twitch/App.js
+++ app-twitch/src/Twitch/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamList from '../components/streams/StreamList';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamDelete from '../components/streams/StreamDelete';
 import StreamShow from '../components/streams/StreamShow';
+import Header from '../components/Header';
 
 function App() {
 	return (
-		<div>
+		<div className="ui container">
 			<BrowserRouter>
 				<div>
+					<Header />
 					<Route path="/" exact component={StreamList} />
 					<Route path="/streams/new" exact component={StreamCreate} />
 					<Route path="/streams/edit" exact component={StreamEdit} />
