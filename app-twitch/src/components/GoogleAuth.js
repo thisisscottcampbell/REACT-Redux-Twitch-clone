7876fab04639c01cad6e4d3f4018b9d272d7c11c
Scott Campbell <stc9486@gmail.com>
2021-03-04 20:32:53 -0500

    gapi init

--- app-twitch/src/components/GoogleAuth.js
+++ app-twitch/src/components/GoogleAuth.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const GoogleAuth = () => {
-	return <div>Google Auth</div>;
-};
+class GoogleAuth extends React.Component {
+	//state = { isSignedIn: null };
+
+	componentDidMount() {
+		window.gapi.load('client:auth2', () => {
+			window.gapi.client.init({
+				clientId:
+					'603737145395-lp6ia8vv2s21i9e9gllriptiuruc3pas.apps.googleusercontent.com',
+				scope: 'email',
+			});
+			// .then(() => {
+			//   this.auth = window.gapi.auth2.getAuthInstance();
+			//   this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+			// });
+		});
+	}
+
+	// renderAuthButton() {
+	//   if (this.state.isSignedIn === null) {
+	//     return <div>I dont know if we are signed in</div>;
+	//   } else if (this.state.isSignedIn) {
+	//     return <div>I am signed in!</div>;
+	//   } else {
+	//     return <div>I am not signed in</div>;
+	//   }
+	render() {
+		return <div>GoogleAuth</div>;
+	}
+}
 
 export default GoogleAuth;
