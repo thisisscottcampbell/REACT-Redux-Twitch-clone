1389a6567eac1dfb02c836d22628c8639e385160
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-10 23:29:17 -0500

    api

--- app-twitch/client/src/components/Header.js
+++ app-twitch/client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GoogleAuth from '../components/GoogleAuth';
+import GoogleAuth from './GoogleAuth';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
