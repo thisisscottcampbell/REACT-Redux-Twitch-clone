3f74eb42a7bd36a5acb295a1518d75d49881d5b5
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-11 00:16:57 -0500

    REST action creators

--- app-twitch/client/src/actions/index.js
+++ app-twitch/client/src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+/* eslint-disable no-unused-vars */
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	GET_STREAM,
+	GET_STREAMS,
+	DELETE_STREAM,
+	EDIT_STREAM,
+} from './types';
 import streams from '../apis/streams';
 
 export const signIn = (userId) => {
@@ -14,6 +23,32 @@ export const signOut = () => {
 	};
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-	streams.post('/streams', formValues);
+export const createStream = (formData) => async (dispatch) => {
+	const res = await streams.post('/streams', formData);
+
+	dispatch({ type: CREATE_STREAM, payload: res.data });
+};
+
+export const getStreams = () => async (dispatch) => {
+	const res = await streams.get('/streams');
+
+	dispatch({ type: GET_STREAMS, payload: res.data });
+};
+
+export const getStream = (id) => async (dispatch) => {
+	const res = await streams.get(`/streams/${id}`);
+
+	dispatch({ type: GET_STREAM, payload: res.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+	await streams.delete(`streams/${id}`);
+
+	dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export const editStream = (id, formData) => async (dispatch) => {
+	const res = await streams.put(`streams/${id}`, formData);
+
+	dispatch({ type: EDIT_STREAM, payload: res.data });
 };
