dcc0f12ea9008ba1ad8523fd20ad5f655b83a3e0
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-18 18:17:10 -0400

    link user to stream via auth

--- app-twitch/client/src/components/streams/StreamList.js
+++ app-twitch/client/src/components/streams/StreamList.js
@@ -7,10 +7,20 @@ class StreamList extends React.Component {
 		this.props.getStreams();
 	}
 
+	renderAdmins(stream) {
+		if (stream.userId === this.props.currUserId)
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">Edit</button>
+					<button className="ui button negative">Delete</button>
+				</div>
+			);
+	}
 	renderList() {
 		return this.props.streams.map((stream) => {
 			return (
 				<div className="item" key={stream.id}>
+					{this.renderAdmins(stream)}
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
@@ -32,7 +42,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-	return { streams: Object.values(state.streams) };
+	return {
+		streams: Object.values(state.streams),
+		currUserId: state.auth.userId,
+	};
 };
 
 export default connect(mapStateToProps, { getStreams })(StreamList);
