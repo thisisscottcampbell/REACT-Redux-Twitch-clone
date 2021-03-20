6d6a6cefa7fd277c9c5fcb293ea5881423de9e91
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-20 19:30:28 -0400

    reusable modal

--- app-twitch/client/src/components/Modal.js
+++ app-twitch/client/src/components/Modal.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = (props) => {
+const Modal = ({ title, content, actions, handleDismiss }) => {
 	return ReactDOM.createPortal(
-		<div
-			onClick={() => history.push('/')}
-			className="ui dimmer modals visible active"
-		>
+		<div onClick={handleDismiss} className="ui dimmer modals visible active">
 			<div
-				onClick={(e) => e.stopPropogation()}
-				className="ui dimmer modals visible active"
+				onClick={(e) => e.stopPropagation()}
+				className="ui standard modals visible active"
 			>
 				<div className="ui standard modal visible active">
-					<div className="header">Delete</div>
-					<div className="content">
-						Are you sure you want to delete this stream?
-					</div>
-					<div className="actions">
-						<button className="ui primary button">Delete</button>
-						<button className="ui button">Cancel</button>
-					</div>
+					<div className="header">{title}</div>
+					<div className="content">{content}</div>
+					<div className="actions">{actions}</div>
 				</div>
 			</div>
 		</div>,
--- app-twitch/client/src/components/streams/StreamDelete.js
+++ app-twitch/client/src/components/streams/StreamDelete.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Modal from '../Modal';
 import ReactDOM from 'react-dom';
+import history from '../../history';
 
 const StreamDelete = () => {
+	const actions = (
+		<React.Fragment>
+			<button className="ui primary button">Delete</button>
+			<button className="ui button">Cancel</button>
+		</React.Fragment>
+	);
+
+	const handleDismiss = () => history.push('/');
+
 	return (
 		<div>
 			Stream Delete
-			<Modal />
+			<Modal
+				title="Delete Stream"
+				content="Are your sure you want to delte this stream?"
+				actions={actions}
+				handleDismiss={handleDismiss}
+			/>
 		</div>
 	);
 };
