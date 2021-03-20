075134cf55b770a703444b96b773c463bdea33ce
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-20 19:56:38 -0400

    del class to functional with useEff

--- app-twitch/client/src/components/streams/StreamDelete.js
+++ app-twitch/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getStream } from '../../actions';
 import Modal from '../Modal';
 import ReactDOM from 'react-dom';
 import history from '../../history';
 
-const StreamDelete = () => {
+const StreamDelete = ({ match, getStream, stream }) => {
 	const actions = (
 		<React.Fragment>
 			<button className="ui primary button">Delete</button>
@@ -13,17 +15,25 @@ const StreamDelete = () => {
 
 	const handleDismiss = () => history.push('/');
 
-	return (
+	useEffect(() => getStream(match.params.id), []);
+
+	return stream ? (
 		<div>
-			Stream Delete
+			Delete Stream
 			<Modal
 				title="Delete Stream"
-				content="Are your sure you want to delte this stream?"
+				content={`Are your sure you want to delete stream: ${stream.title}?`}
 				actions={actions}
 				handleDismiss={handleDismiss}
 			/>
 		</div>
+	) : (
+		<div>...loading</div>
 	);
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { getStream })(StreamDelete);
