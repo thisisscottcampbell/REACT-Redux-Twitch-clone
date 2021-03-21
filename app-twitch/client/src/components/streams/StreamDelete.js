aaa1879a3f2928c05e8ebac65a9bfa21b74c918f
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-20 20:28:35 -0400

    delete, REST complete

--- app-twitch/client/src/components/streams/StreamDelete.js
+++ app-twitch/client/src/components/streams/StreamDelete.js
@@ -1,34 +1,38 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getStream } from '../../actions';
+import { Link } from 'react-router-dom';
+import { getStream, deleteStream } from '../../actions';
 import Modal from '../Modal';
 import ReactDOM from 'react-dom';
 import history from '../../history';
 
-const StreamDelete = ({ match, getStream, stream }) => {
+const StreamDelete = ({ match, getStream, deleteStream, stream }) => {
+	const { id } = match.params;
+
 	const actions = (
 		<React.Fragment>
-			<button className="ui primary button">Delete</button>
-			<button className="ui button">Cancel</button>
+			<button onClick={() => deleteStream(id)} className="ui primary button">
+				Delete
+			</button>
+			<Link to="/" className="ui button">
+				Cancel
+			</Link>
 		</React.Fragment>
 	);
 
 	const handleDismiss = () => history.push('/');
 
-	useEffect(() => getStream(match.params.id), []);
+	useEffect(() => getStream(id), []);
 
 	return stream ? (
-		<div>
-			Delete Stream
-			<Modal
-				title="Delete Stream"
-				content={`Are your sure you want to delete stream: ${stream.title}?`}
-				actions={actions}
-				handleDismiss={handleDismiss}
-			/>
-		</div>
+		<Modal
+			title="Delete Stream"
+			content={`Are your sure you want to delete stream: ${stream.title}?`}
+			actions={actions}
+			handleDismiss={handleDismiss}
+		/>
 	) : (
-		<div>...loading</div>
+		<div>...getting your stream</div>
 	);
 };
 
@@ -36,4 +40,6 @@ const mapStateToProps = (state, ownProps) => {
 	return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { getStream })(StreamDelete);
+export default connect(mapStateToProps, { getStream, deleteStream })(
+	StreamDelete
+);
